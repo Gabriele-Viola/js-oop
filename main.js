8210834bb6aee63bc53f41dc6817199e4c631486
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-22 14:58:39 +0200

    add extended class n more upgrade function

--- main.js
+++ main.js
@@ -21,3 +21,48 @@ cicla nell'array usando un for
 crea una nuova array che contenga solo il nome dei veicoli
 crea una nuova array con solo i veicoli elettriche*/
 console.log('hi');
+
+class Veicolo {
+    marca;
+    anno;
+    colore;
+
+    constructor(brand, year, color) {
+        this.marca=brand;
+        this.anno=year;
+        this.colore=color;
+
+        
+    }
+
+    getinformation(){
+        return `the vehicle is a ${this.marca} mada in ${this.anno} in this color: ${this.colore}`
+    }
+    getage(){  
+        return new Date().getFullYear() - this.anno  
+    }
+}
+
+const punto = new Veicolo ( 'FIAT', 2019, 'blu')
+console.log(punto);
+console.log(punto.getinformation());
+console.log(punto.getage());
+
+class Automobile extends Veicolo {
+    constructor(brand, year, color, doors, fuels) {
+       super(brand, year, color)
+       this.porte = doors
+       this.carburante = fuels
+        
+    }
+    getMoreInfo(doors, fuels){
+        return `${this.getinformation()}, with ${this.porte} and ${this.carburante} engine`
+    }
+}
+const corsa = new Automobile('Opel', 2020, 'nera','4 porte', 'benzina')
+console.log(corsa.carburante);
+console.log(corsa.getage());
+console.log(corsa.getinformation());
+console.log(corsa.getMoreInfo());
+
+
